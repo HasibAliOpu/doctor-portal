15e11b8da8a76133ba3ffa863605367c1cb06ee1
H_A _Opu <wwwhasib4@gmail.com>
2022-05-10 13:28:00 +0600

    added contact form on home page

---
+++ src/Pages/Home/ContactForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import appointment from "../../assets/images/appointment.png";
+import GradientButton from "../Shared/GradientButton";
+
+const ContactForm = () => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${appointment})` }}
+      className="min-h-screen flex flex-col justify-center items-center gap-2"
+    >
+      <div className="text-center mb-5 px-3 lg:px-0">
+        <h3 className="text-primary text-2xl">Contact Us</h3>
+        <h1 className="text-white text-4xl">Stay connected with us</h1>
+      </div>
+      <form className="px-8 lg:px-0">
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="input input-bordered input-info w-full max-w-xl"
+        />
+        <input
+          type="text"
+          placeholder="Subject"
+          className="input input-bordered input-info w-full max-w-xl my-3"
+        />
+        <textarea
+          type="text"
+          placeholder="Your message"
+          className="input input-bordered input-info w-full max-w-xl mb-2"
+        />
+      </form>
+      <GradientButton>Submit</GradientButton>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,7 @@ import Banner from "./Banner";
 import Info from "./Info";
 import Services from "./Services";
 import PatientReviews from "./PatientReviews";
+import ContactForm from "./ContactForm";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Services />
       <MakeAppointment />
       <PatientReviews />
+      <ContactForm />
     </div>
   );
 };
