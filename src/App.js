7052229283ce6953188eb15f136bafb89678882d
H_A _Opu <wwwhasib4@gmail.com>
2022-05-12 10:38:03 +0600

    added login form using react hook form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Appointment from "./Pages/Appointment/Appointment";
 import Home from "./Pages/Home/Home";
 import Navbar from "./Pages/Shared/Navbar";
 import Login from "./Pages/Login/Login";
+import Register from "./Pages/Login/Register";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/appointment" element={<Appointment />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="w-100">
+      <img
+        src="https://i.pinimg.com/originals/f9/41/ae/f941ae9d16fd7d2957eea6e5b1100d1e.gif"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Loading;
