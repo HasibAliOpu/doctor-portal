c30e59c6100449db37da29798507aff3f84d55d2
H_A _Opu <wwwhasib4@gmail.com>
2022-05-18 02:53:04 +0600

    added new doctor info on database

--- src/Pages/Dashboard/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor.js
@@ -1,6 +1,9 @@
+import { async } from "@firebase/util";
+import axios from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useQuery } from "react-query";
+import { toast } from "react-toastify";
 import Loading from "../../Loading";
 
 const AddDoctor = () => {
@@ -8,13 +11,67 @@ const AddDoctor = () => {
     register,
     formState: { errors },
     handleSubmit,
+    reset,
   } = useForm();
   const { data: services, isLoading } = useQuery("services", () =>
     fetch("http://localhost:5000/service").then((res) => res.json())
   );
+
+  const imageStorageKey = "e1a7ceffc9c7ffb3c42389531835f4bb";
+
+  /*
+   * we can store images in 3 ways
+   * 1. Third party storage
+   * 2.your own storage in your own server
+   * 3.Database: MongoDB
+   *
+   * YUP: to validate file
+   */
+
   const onSubmit = async (data) => {
-    console.log(data);
+    const image = data.image[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
+
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.success) {
+          const img = result.data.url;
+          const doctor = {
+            name: data.name,
+            email: data.email,
+            specialty: data.specialty,
+            img: img,
+          };
+          /* ** send to database  ** */
+          (async () => {
+            const { data } = await axios.post(
+              "http://localhost:5000/doctor",
+              doctor,
+              {
+                headers: {
+                  authorization: `Bearer ${localStorage.getItem(
+                    "accessToken"
+                  )}`,
+                },
+              }
+            );
+            if (data.acknowledged) {
+              toast.success("Successfully Added Doctor");
+              reset();
+            } else {
+              toast.error("Something was Wrong");
+            }
+          })();
+        }
+      });
   };
+
   if (isLoading) {
     return <Loading />;
   }
@@ -82,20 +139,40 @@ const AddDoctor = () => {
                   )}
                 </label>
               </div>
-              <div className="form-control w-full max-w-xs relative">
+              <div className="form-control w-full max-w-xs pb-6">
                 <label className="label">
                   <span className="label-text">Specialty</span>
                 </label>
-                <select className="select select-bordered w-full max-w-xs">
+                <select
+                  {...register("specialty")}
+                  className="select select-bordered w-full max-w-xs"
+                >
                   {services.map((s) => (
-                    <option key={s._id}>{s.name}</option>
+                    <option key={s._id} defaultValue={s.name}>
+                      {s.name}
+                    </option>
                   ))}
                 </select>
+              </div>
 
+              <div className="form-control w-full max-w-xs">
                 <label className="label">
-                  {errors.specialty?.type === "required" && (
+                  <span className="label-text">Photo</span>
+                </label>
+                <input
+                  type="file"
+                  className="input input-bordered w-full max-w-xs"
+                  {...register("image", {
+                    required: {
+                      value: true,
+                      message: "Image is Required",
+                    },
+                  })}
+                />
+                <label className="label">
+                  {errors.image?.type === "required" && (
                     <span className="label-text-alt text-red-500">
-                      {errors.specialty?.message}
+                      {errors.image.message}
                     </span>
                   )}
                 </label>
@@ -104,7 +181,7 @@ const AddDoctor = () => {
               <input
                 className="btn w-full max-w-xs text-white"
                 type="submit"
-                value="Register"
+                value="add"
               />
             </form>
           </div>
